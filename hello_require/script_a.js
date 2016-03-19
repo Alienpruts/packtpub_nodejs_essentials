dacbb61b8b8cff4cb4cf0b97bfef69b0a32461b3
Bert Van de Casteele <bert@3sign.com>
2016-03-19 10:45:18 +0100

    Added hello_require example 1

---
+++ hello_require/script_a.js
@@ -0,0 +1,6 @@
+/**
+ * Created by bert on 3/19/16.
+ */
+function run () {
+    console.log("I'm running from script_a");
+}
---
+++ hello_require/script_b.js
@@ -0,0 +1,7 @@
+/**
+ * Created by bert on 3/19/16.
+ */
+function run () {
+    console.log("I'm running from script_b");
+}
+
