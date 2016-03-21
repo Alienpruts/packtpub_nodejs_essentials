cbfd189f242f60dd3a5bebb44ed9a0b4c0b67ede
Bert Van de Casteele <bert@3sign.com>
2016-03-21 23:20:03 +0100

    Added Request handler to server.js

--- simple_http/server.js
+++ simple_http/server.js
@@ -2,8 +2,21 @@
  * Created by bert on 3/21/16.
  */
 var Http = require('http');
-var server = Http.createServer();
+var server = Http.createServer(requestHandler);
+var count = 0
 
 server.listen(3000, function () {
     console.log("listening on port 3000");
 });
+
+function requestHandler(request, response) {
+    var message;
+    count += 1;
+    response.writeHead(201, {
+        'Content-Type': 'text/plain',
+    });
+
+    message = 'Visitor count: ' + count;
+    console.log(message);
+    response.end(message);
+}
