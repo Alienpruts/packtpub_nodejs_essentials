5c33904576505b23cfdbc6792936c3462aded8d0
Bert Van de Casteele <bert@3sign.com>
2016-03-22 22:32:15 +0100

    Added a secondary way of issuing a POST request

---
+++ simple_http/post_test.js
@@ -0,0 +1,17 @@
+/**
+ * Created by bert on 3/22/16.
+ */
+
+var request = require('request');
+
+request({
+    method: 'POST',
+    uri: 'http://localhost:3000/message',
+    body: {'name': 'bert'},
+    json: true,
+}, function (error, response, body) {
+    //console.log('code: ' + response.statusCode);
+    console.log(body);
+    console.log(response);
+    //console.log(error);
+});
