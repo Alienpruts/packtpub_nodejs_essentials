cde3d5c91b2d6bf65bab5178e1fdd64adb03ebfa
Bert Van de Casteele <bert@3sign.com>
2016-03-22 22:56:17 +0100

    Fixed post_test for correct JSON

--- simple_http/post_test.js
+++ simple_http/post_test.js
@@ -7,11 +7,11 @@ var request = require('request');
 request({
     method: 'POST',
     uri: 'http://localhost:3000/message',
-    body: {'name': 'bert'},
+    body: {'message': 'bert'},
     json: true,
 }, function (error, response, body) {
     //console.log('code: ' + response.statusCode);
-    console.log(body);
-    console.log(response);
-    //console.log(error);
+    //console.log(body);
+    //console.log(response);
+    console.log(error);
 });
