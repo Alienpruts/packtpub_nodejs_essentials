92a3946e876daa9bc494b96ec092cd64c25e48ee
Bert Van de Casteele <bert@3sign.com>
2016-03-23 17:53:14 +0100

    Added JSONWebToken + implementation

--- basic-auth/basic-auth.js
+++ basic-auth/basic-auth.js
@@ -21,6 +21,8 @@ var Passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 var Express = require('express');
 var BodyParser = require('body-parser');
+var JSONWebToken = require('jsonwebtoken');
+var Crypto = require('crypto');
 
 var options = {
     usernameField: 'username',
@@ -44,6 +46,38 @@ var localStrategy = new LocalStrategy(options, function (username, password, don
     done(null, user);
 });
 
+// Generate JSONWebtoken for use in further requests.
+var generateToken = function(request, response) {
+    // Payload contains user id + username. Verify if claim is correct.
+    var payload = {
+        id: user.id,
+        username: user.username,
+    };
+
+    // Generate random string
+    var secret = Crypto.randomBytes(128).toString('base64');
+
+    // Create token using payload + secret (hashing?)
+    var token = JSONWebToken.sign(payload, secret);
+
+    // Set secret in user object (normally, would be DB)
+    request.user = user;
+    request.user.secret = secret;
+
+    return token;
+};
+
+var generateTokenHandler = function(request, response) {
+    var user = request.user;
+
+    // Generate token.
+    var token = generateToken(user);
+
+    console.log(token);
+    // Return the user a token to use.
+    response.send(token);
+};
+
 // Use the Localstrategy in our 'master' Passport object
 Passport.use('local', localStrategy);
 
@@ -54,9 +88,7 @@ app.use(Passport.initialize());
 app.post(
     '/login',
     Passport.authenticate('local', {session: false}),
-    function (request, response) {
-        response.send('User ID' + request.user.id);
-    }
+    generateTokenHandler
 );
 
 app.listen(3000, function () {
