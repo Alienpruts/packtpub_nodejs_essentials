cc57ba3cec272d705a280f514f279519f7041fa7
Bert Van de Casteele <bert@3sign.com>
2016-03-23 08:01:55 +0100

    Initial commit basic-auth.js | added users object

---
+++ basic-auth/basic-auth.js
@@ -0,0 +1,20 @@
+/**
+ * Created by bert on 3/23/16.
+ */
+
+var users = {
+    foo: {
+        username: 'foo',
+        password: 'bar',
+        id: 1,
+    }
+
+    bar: {
+        username: 'bar',
+        password: 'foo',
+        id: 2,
+    }
+};
+
+
+
