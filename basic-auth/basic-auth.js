f24279fabba380556aef750819089a2b14fd2095
Bert Van de Casteele <bert@3sign.com>
2016-03-23 18:08:24 +0100

    Added VerifyToken functionality

--- basic-auth/basic-auth.js
+++ basic-auth/basic-auth.js
@@ -23,6 +23,7 @@ var Express = require('express');
 var BodyParser = require('body-parser');
 var JSONWebToken = require('jsonwebtoken');
 var Crypto = require('crypto');
+var BearerStrategy = require('passport-http-bearer').Strategy;
 
 var options = {
     usernameField: 'username',
@@ -78,8 +79,36 @@ var generateTokenHandler = function(request, response) {
     response.send(token);
 };
 
+var verifyToken = function (token, done) {
+    var payload = JSONWebToken.decode(token);
+    var user = users[payload.username];
+
+    // If user does not exists, or false credentials are give : false.
+    if (user == null ||
+        user.id !== payload.id ||
+        user.username !== payload.username) {
+        return done(null, false);
+    }
+
+    // Ensure the token is valid now we have a user.
+    JSONWebToken.verify(token, user.secret, function (error, decoded) {
+        if (error || decoded == null) {
+            return done(error, false);
+        }
+
+        // We have a user and token is verified, user has been cleared.
+        return done(null, user);
+    })
+};
+
+var bearerStrategy = new BearerStrategy(
+    verifyToken
+);
+
 // Use the Localstrategy in our 'master' Passport object
 Passport.use('local', localStrategy);
+// Use the BearerStrategy.
+Passport.use('bearer', bearerStrategy);
 
 app.use(BodyParser.urlencoded({extended: false}));
 app.use(BodyParser.json());
@@ -89,6 +118,19 @@ app.post(
     '/login',
     Passport.authenticate('local', {session: false}),
     generateTokenHandler
+);
+
+// Create a alternative route to test bearer strategy login with.
+app.get(
+    '/userinfo',
+    Passport.authenticate('bearer', {session: false}),
+    function (request, response) {
+        var user = request.user;
+        response.send({
+            id: user.id,
+            username: user.username,
+        });
+    }
 );
 
 app.listen(3000, function () {
