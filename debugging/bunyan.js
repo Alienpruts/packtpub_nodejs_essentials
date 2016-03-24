579ef6ce33bd2baebf87876df34e35c5dfceb6df
Bert Van de Casteele <bert@3sign.com>
2016-03-24 19:19:26 +0100

    Added writing to streams to Bunyan logger

--- debugging/bunyan.js
+++ debugging/bunyan.js
@@ -6,8 +6,22 @@ var logger;
 
 logger = Bunyan.createLogger({
     name: 'example-8',
-    level: Bunyan.TRACE,
+    streams: [
+        // to file.
+        {
+            level: Bunyan.INFO,
+            path: './log.log',
+        },
+        // To display stdout.
+        {
+            level: Bunyan.INFO,
+            stream: process.stdout,
+        }
+    ],
 });
+
+logger.info(process.versions);
+logger.info('Application started');
 
 /*
 logger.trace('Trace');
@@ -20,10 +34,10 @@ logger.fatal('Fatal');
 logger.fatal('We have a fatal, ABORT ABORT');
 */
 
-try {
+/*try {
     ref.go();
 } catch (error) {
     logger.error(error);
-}
+}*/
 
-process.exit(1);
+//process.exit(1);
