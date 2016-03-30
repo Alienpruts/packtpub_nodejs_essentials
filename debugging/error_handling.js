4588647296aeb9ec752830a379bdef76c8b89777
Bert Van de Casteele <bert@3sign.com>
2016-03-30 08:01:30 +0200

    Added simple uncaught exception handling

--- debugging/error_handling.js
+++ debugging/error_handling.js
@@ -1,7 +1,26 @@
 /**
  * Created by bert on 3/30/16.
  */
+var Bunyan = require('bunyan');
+var logger;
 var Q = require('q');
+var fs = require('fs');
+
+logger = Bunyan.createLogger({
+    name: 'example-8',
+    streams: [
+        // to file.
+        {
+            level: Bunyan.INFO,
+            path: './log.log',
+        },
+        // To display stdout.
+        {
+            level: Bunyan.INFO,
+            stream: process.stdout,
+        }
+    ],
+});
 
 function parseJSONAndUse(input) {
     var json = null;
@@ -16,4 +35,19 @@ function parseJSONAndUse(input) {
     return Q(use(json));
 }
 
+// Error handler example.
+process.on('uncaughtException', function errorProcessHandler(error) {
+    logger.fatal(error);
+    logger.fatal('Fatal error encountered, exiting now');
+    process.exit(1);
+});
+
+// Testing calling code for error handling.
 console.log(parseJSONAndUse('it will not work'));
+
+// Testing code for throwing uncaught exception.
+fs.readFile('idonotexist.txt', function (error, data) {
+    if (error) {
+        throw error;
+    }
+});
