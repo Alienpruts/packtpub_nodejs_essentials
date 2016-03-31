a409edeacec57151031c4ee637f8d74b98feaee4
Bert Van de Casteele <bert@3sign.com>
2016-03-31 07:57:16 +0200

    Modified config example for using multiple servers

--- configuration/json_config.js
+++ configuration/json_config.js
@@ -5,6 +5,7 @@ var Config = require('./config.json');
 var Http = require('http');
 
 Http.createServer(function (request, response) {
-}).listen(Config.port, Config.host, function () {
-    console.log('Listening on port', Config.port, 'and host', Config.host);
+}).listen(Config.server.port, Config.server.host, function () {
+    console.log('Listening on port', Config.server.port, 'and host', Config.server.host);
+    console.log('\nAlso (fictively) contacting Database server on port', Config.database.port, 'on host', Config.database.host);
 });
